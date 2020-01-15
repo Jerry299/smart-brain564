12e86c8c9a31041c0cfbea2afe553ba0ff71adab
Jerry299 <josephjeremiah564@yahoo.com>
2020-01-15 04:42:48 +0100

    added procees.env.PORT for server

--- backend/server.js
+++ backend/server.js
@@ -179,6 +179,6 @@ app.put("/image", (req, res) => {
   } */
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is running on port ${process.env.PORT}");
 });
