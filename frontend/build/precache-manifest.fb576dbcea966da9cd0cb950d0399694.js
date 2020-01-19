048cdf345b87f91f42af4fb04ddc82d1f8d1ae86
Jerry299 <josephjeremiah564@yahoo.com>
2020-01-19 04:53:54 +0100

    build added

---
+++ frontend/build/precache-manifest.fb576dbcea966da9cd0cb950d0399694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2003ec3edf5d4d40913d31b1185212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51744840a8d70ac5c93a",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "57bf44d783590661ef51",
+    "url": "/static/css/main.70ad235e.chunk.css"
+  },
+  {
+    "revision": "51744840a8d70ac5c93a",
+    "url": "/static/js/2.6f11e6e7.chunk.js"
+  },
+  {
+    "revision": "57bf44d783590661ef51",
+    "url": "/static/js/main.9ff4b5f1.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
